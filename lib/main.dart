8deed0a3b924a3d61325ad5eac5232e5205ee815
ayush572 <ayushdagaeinstein@gmail.com>
2023-11-02 10:07:20 +0530

    Added the integration for the firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,26 @@
 import 'package:ecom_app/screens/sign_in_screen.dart';
 import 'package:ecom_app/utils/color_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  //if the device is android or web
+  if(kIsWeb){
+
+    //await keyword added as it's a future
+    await Firebase.initializeApp(options: const FirebaseOptions(
+        apiKey: "AIzaSyCyVJ72OX_xdKL9BEkBYIGonlwqAlgukAE",
+        authDomain: "fypecom-123ae.firebaseapp.com",
+        projectId: "fypecom-123ae",
+        storageBucket: "fypecom-123ae.appspot.com",
+        messagingSenderId: "699622495862",
+        appId: "1:699622495862:web:a81f6a60fd8bd2a174b157",
+        measurementId: "G-J3W0Y9HPD5"
+    ));
+  }else{
+    Firebase.initializeApp();
+  }
   runApp(const Ecomfyp());
 }
 
